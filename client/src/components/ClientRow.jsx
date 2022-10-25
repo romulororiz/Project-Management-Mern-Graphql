e42a96df4f56f97ce25fd667af68a19c32aa6673
Romulo Roriz <romulonmroriz@gmail.com>
2022-10-25 22:30:01 +0200

    Delete client with useMutation hook

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,7 +1,28 @@
+import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 const ClientRow = ({ client }) => {
-	const { name, email, phone } = client;
+	const { id, name, email, phone } = client;
+
+	const [deleteClient] = useMutation(DELETE_CLIENT, {
+		variables: {
+			id: id,
+		},
+		//* Refetch GET_CLIENTS so it's gone from the UI
+		// refetchQueries: [{ query: GET_CLIENTS }],
+		//* Get clients from the cache and updates it so it's gone from the UI (no new requests)
+		update(cache, { data: { deleteClient } }) {
+			const { clients } = cache.readQuery({ query: GET_CLIENTS });
+			cache.writeQuery({
+				query: GET_CLIENTS,
+				data: {
+					clients: clients.filter(client => client.id !== deleteClient.id),
+				},
+			});
+		},
+	});
 
 	return (
 		<tr>
@@ -9,7 +30,7 @@ const ClientRow = ({ client }) => {
 			<td>{email}</td>
 			<td>{phone}</td>
 			<td>
-				<button className='btn btn-danger btn-sm'>
+				<button className='btn btn-danger btn-sm' onClick={deleteClient}>
 					<FaTrash />
 				</button>
 			</td>
