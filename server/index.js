771fd9521ca1d4f4b51dce7ada6c428fe5d48059
Romulo Roriz <romulonmroriz@gmail.com>
2022-10-25 22:05:15 +0200

    Handle Cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 require('colors');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
@@ -9,6 +10,8 @@ const app = express();
 
 // Connect to Database
 connectDB();
+
+app.use(cors());
 
 app.use(
 	'/graphql',
