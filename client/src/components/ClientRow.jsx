d3cb96b028fbe178075d7f1915f28083421a046f
Romulo Roriz <romulonmroriz@gmail.com>
2022-10-26 13:18:42 +0200

    Delete client and projects attached to that client

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 const ClientRow = ({ client }) => {
 	const { id, name, email, phone } = client;
@@ -11,17 +12,17 @@ const ClientRow = ({ client }) => {
 			id: id,
 		},
 		//* Refetch GET_CLIENTS so it's gone from the UI
-		// refetchQueries: [{ query: GET_CLIENTS }],
+		refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
 		//* Get clients from the cache and updates it so it's gone from the UI (no new requests)
-		update(cache, { data: { deleteClient } }) {
-			const { clients } = cache.readQuery({ query: GET_CLIENTS });
-			cache.writeQuery({
-				query: GET_CLIENTS,
-				data: {
-					clients: clients.filter(client => client.id !== deleteClient.id),
-				},
-			});
-		},
+		// update(cache, { data: { deleteClient } }) {
+		// 	const { clients } = cache.readQuery({ query: GET_CLIENTS });
+		// 	cache.writeQuery({
+		// 		query: GET_CLIENTS,
+		// 		data: {
+		// 			clients: clients.filter(client => client.id !== deleteClient.id),
+		// 		},
+		// 	});
+		// },
 	});
 
 	return (
