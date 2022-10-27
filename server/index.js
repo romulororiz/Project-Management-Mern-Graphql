f1f7ff20323f3e5811519905d621c4caf58b299c
Romulo Roriz <romulonmroriz@gmail.com>
2022-10-27 17:11:04 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -28,11 +28,11 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(__dirname, '../', 'client', 'build', 'index.html');
+		res.sendFile(__dirname, '../client/src/build/index.html');
 	});
 } else {
 	app.get('/', (req, res) => {
-		res.status(200).json({ message: 'Welcome to the Project Mgmt App' });
+		res.status(200).json({ message: 'Welcome to the ProjectMgmt App' });
 	});
 }
 
