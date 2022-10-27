76b2c64015aa254594d5c611370b4b40d5996796
Romulo Roriz <romulonmroriz@gmail.com>
2022-10-27 22:06:03 +0200

    Prepare for deploy

--- server/index.js
+++ server/index.js
@@ -22,18 +22,12 @@ app.use(
 );
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-	// Set build folder as static
-	app.use(express.static(path.join(__dirname, '../client/build')));
+// Set build folder as static
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-	app.get('*', (req, res) => {
-		res.sendFile(__dirname, '../client/src/build/index.html');
-	});
-} else {
-	app.get('/', (req, res) => {
-		res.status(200).json({ message: 'Welcome to the Support Desk API' });
-	});
-}
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
